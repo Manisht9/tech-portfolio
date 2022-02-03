5ae7e7526f18ca9f92e48ebd8b3c1c6760b53954
Tiwaray <74869679+Manisht9@users.noreply.github.com>
2022-02-03 13:57:13 +0530

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Manish Tiwari', // Title for your website.
-    tagline: 'Continuously improving,Open Source GIS Enthusiast,Geospatial,WebGIS,Spatial Data Science.',
+    tagline: 'Hi! ✋🏻 I’m Remote Sensing & GIS Expert,Open Source GIS Enthusiast,Aspiring Spatial Data Scientist.Thanks For Visting My Portfolio',
     profilePhoto: 'img/profile.png',
     url: 'https://Manisht9.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
